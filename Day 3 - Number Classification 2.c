19454770d072138c4d8c3ffed103c6b08ec32abb
AyushDhakad023 <ayushdhakad7917@gmail.com>
2024-10-04 20:06:38 +0530

    Add files via upload

---
+++ Day 3 - Number Classification 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("enter a numbr");
+    scanf("%d",&num);
+    if(num/2==0){
+        printf("number is even");
+    }
+    else{
+        printf("number is odd");
+        
+    }
+    return 0;
+}
