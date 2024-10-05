0f8737b446e5185f15ab0896ca086f8caf4189f8
AyushDhakad023 <ayushdhakad7917@gmail.com>
2024-10-05 22:42:20 +0530

    Add files via upload

---
+++ Day 4 - Classification of grades.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int grade=60;
+    if(grade>=90) {
+        printf("grade:A.\n");
+    }else if (grade>=80){
+        printf("grade:B.\n");
+    }else if(grade>=70){
+        printf("grade:C.\n");
+    }else{
+        printf("grade:F\n");
+        
+    }
+    return 0;
+    
+}
